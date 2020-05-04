64c7b25298cbc28159a64df2960fa8296fd8bb76
Michele Biondi <michelebiondi01@gmail.com>
2020-05-04 18:50:37 +0200

    Add message services for finishing fuding campaign

--- message-broker/index.js
+++ message-broker/index.js
@@ -3,3 +3,5 @@ const { createMessageSendHandler } = require('./createMessageSendListener');
 createMessageSendHandler('Service_Message_FundingRequestReceived', 'Message_FundingRequestReceived', ['amount', 'requestCorrelationId']);
 createMessageSendHandler('Service_Message_AcceptedRequest', 'Message_AcceptedRequest', [], ['requestCorrelationId']);
 createMessageSendHandler('Service_Message_RefusedRequest', 'Message_RefusedRequest', [], ['requestCorrelationId']);
+createMessageSendHandler('Service_Message_CampaignSucceeded', 'Message_CampaignSucceeded', [], ['requestCorrelationId']);
+createMessageSendHandler('Service_Message_CampaignFailed', 'Message_CampaignFailed', [], ['requestCorrelationId']);
