78d6d47ef5583afc211b90dc7e34c8781c3ca851
Michele Biondi <michelebiondi01@gmail.com>
2020-05-04 17:24:20 +0200

    Improved message handler fun and handled accepted request

--- message-broker/createMessageSendListener.js
+++ message-broker/createMessageSendListener.js
@@ -6,25 +6,35 @@ const rest_endpoint = process.env.REST_ENDPOINT || 'rest';
 
 const config = { baseUrl: `http://${camunda_endpoint}:8080/${rest_endpoint}`, use: logger };
 
-exports.createMessageSendHandler = (topic, messageName, variables = []) => {
+exports.createMessageSendHandler = (topic, messageName, variables = [], correlationVariables = []) => {
     const client = new Client(config);
 
 
     client.subscribe(topic, async function ({ task, taskService }) {
         const key = task.businessKey;
-        console.log(task.variables.getTyped('amount'));
 
         const processVariables = {};
+
         variables.forEach((variable) => {
             processVariables[variable] = task.variables.getTyped(variable);
         });
+
+        const correlationKeys = {};
+
+        correlationVariables.forEach((variable) => {
+            const { valueInfo, ...correlationKey } = task.variables.getTyped(variable);
+            correlationKeys[variable] = correlationKey;
+        });
+
         console.log(processVariables);
+        console.log(correlationKeys);
 
         const requestBody = {
             messageName: messageName,
             businessKey: key,
             processVariables: processVariables,
-        }
+            correlationKeys: correlationKeys,
+        };
 
         try {
             await axios.post(`http://${camunda_endpoint}:8080/${rest_endpoint}/message`, requestBody);
--- message-broker/index.js
+++ message-broker/index.js
@@ -1,3 +1,4 @@
 const { createMessageSendHandler } = require('./createMessageSendListener');
 
-createMessageSendHandler('Service_Message_FundingRequestReceived', 'Message_FundingRequestReceived', ['amount']);
+createMessageSendHandler('Service_Message_FundingRequestReceived', 'Message_FundingRequestReceived', ['amount', 'requestCorrelationId']);
+createMessageSendHandler('Service_Message_AcceptedRequest', 'Message_AcceptedRequest', [], ['requestCorrelationId']);
