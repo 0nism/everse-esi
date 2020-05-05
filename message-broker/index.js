b03c8a80be35cd2293447a5f2fd9ee5be3b934a8
Michele Biondi <michelebiondi01@gmail.com>
2020-05-05 15:22:48 +0200

    Add asset name for reference

--- message-broker/index.js
+++ message-broker/index.js
@@ -1,6 +1,6 @@
 const { createMessageSendHandler } = require('./createMessageSendListener');
 
-createMessageSendHandler('Service_Message_FundingRequestReceived', 'Message_FundingRequestReceived', ['amount', 'requestCorrelationId']);
+createMessageSendHandler('Service_Message_FundingRequestReceived', 'Message_FundingRequestReceived', ['amount', 'requestCorrelationId', 'assetName']);
 createMessageSendHandler('Service_Message_AcceptedRequest', 'Message_AcceptedRequest', [], ['requestCorrelationId']);
 createMessageSendHandler('Service_Message_RefusedRequest', 'Message_RefusedRequest', [], ['requestCorrelationId']);
 createMessageSendHandler('Service_Message_CampaignSucceeded', 'Message_CampaignSucceeded', [], ['requestCorrelationId']);
